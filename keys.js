22ad951137f59794c0eba1d2e0d539a9f68a5599
Colin Stone <colin.stone.career@gmail.com>
2017-04-01 07:28:57 -0700

    Initial upload- basic file structure, commands not coded

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: 	'A9a6cAXge2TtR6x1ADJ4JUTPc',
+	consumer_secret: 'EqEzEMTncKe6T4pClufMW98BwwNASyEQZIfUBndDYlb7IFYRfN',
+	access_token_key: '848171493049552896-OLmDGlmZ8PfW4t4farkTdtMJ8BStn0l',
+	access_token_secret: 'CBeiTOtm7nqhnew4iIVMcAigu9ddYuGxF8KjhP4sU9aA1',
+}
---
+++ liri.js
@@ -0,0 +1,27 @@
+var keys = require(./"keys.js");
+
+var request = require("request");
+
+var spotify = require("spotify");
+
+var twitter = require("twitter");
+
+var userInput = process.argv[2];
+
+var userInput2 = process.argv[3];
+
+if (userInput === "my-tweets") {
+
+};
+
+if (userInput === "spotify-this-song") {
+
+};
+
+if (userInput === "movie-this") {
+
+};
+
+if (userInput == "do-what-it-says") {
+
+}
